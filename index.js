cdf0600184087fc709f8be132dec190147c9499d
orest0 <orest000@gmail.com>
2024-10-09 23:38:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5];
+
+function recursiveIteration(array, index = 0) {
+  if (index < array.length) {
+    console.log(array[index]);
+    recursiveIteration(array, index + 1);
+  }
+}
+
+recursiveIteration(arr);
